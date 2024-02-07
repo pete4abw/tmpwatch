c6a47232a72f782bcd28b7eab0d694223197ecef
Peter Hyman <pete@peterhyman.com>
2024-02-07 13:10:30 -0600

    Update autogen.sh to clone gnulib if needed

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 
+# Fetch gnulib-tool app. If not found, clone gnulib
 if [ ! -x gnulib/gnulib-tool ] ; then
-	echo "gnulib-tool not found. Was gnulib git fetched?"
-	echo "aborting autogen.sh."
-	exit 1
+	# Is this a git repo or a tarball extracted?
+	if [ -d .git ]; then
+		# we are in a git repo, so fetch the gnulib submodule
+		echo "Executing git submodule update --init"
+		git submodule update --init gnulib
+		if [ $? -ne 0 ] ; then
+			echo "Could not clone submodule"
+			exit 1
+		fi
+	else
+		# we are not in a git repo so clone the gnulib repo
+		echo "Cloning git clone https://git.savannah.gnu.org/git/gnulib.git"
+		git clone https://git.savannah.gnu.org/git/gnulib.git
+		if [ $? -ne 0 ] ; then
+			echo "Could not clone submodule"
+			exit 1
+		fi
+	fi
 fi
 
 echo "Fetching gnulib modules"
