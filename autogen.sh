add3320a4819ad8e1b909bacb3f44601848b4d15
Peter Hyman <pete@peterhyman.com>
2024-03-17 07:09:11 -0500

    Update autotools files to eliminate gnulib submodule

--- autogen.sh
+++ autogen.sh
@@ -1,29 +1,4 @@
-#!/bin/sh
-
-# Fetch gnulib-tool app. If not found, clone gnulib
-if [ ! -x gnulib/gnulib-tool ] ; then
-	# Is this a git repo or a tarball extracted?
-	if [ -d .git ]; then
-		# we are in a git repo, so fetch the gnulib submodule
-		echo "Executing git submodule update --init"
-		git submodule update --init gnulib
-		if [ $? -ne 0 ] ; then
-			echo "Could not clone submodule"
-			exit 1
-		fi
-	else
-		# we are not in a git repo so clone the gnulib repo
-		echo "Cloning git clone https://git.savannah.gnu.org/git/gnulib.git"
-		git clone https://git.savannah.gnu.org/git/gnulib.git
-		if [ $? -ne 0 ] ; then
-			echo "Could not clone submodule"
-			exit 1
-		fi
-	fi
-fi
-
-echo "Fetching gnulib modules"
-gnulib/gnulib-tool --import --dir=. --lib=libgnu --source-base=lib --m4-base=m4 --doc-base=doc --aux-dir=admin --no-libtool --macro-prefix=gl clock-time getopt-gnu progname stpcpy strtoimax xalloc
+#!/bin/bash
 
 echo 'Running autoreconf -if...'
 autoreconf -if || exit 1
